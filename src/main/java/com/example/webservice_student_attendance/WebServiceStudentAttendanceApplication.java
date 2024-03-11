766fe4644ad59f36eee9de0a970bd98e65bb677a
Anton <antontokmakov757@gmail.com>
2024-03-11 19:17:45 +0700

    Started project

---
+++ src/main/java/com/example/webservice_student_attendance/WebServiceStudentAttendanceApplication.java
@@ -0,0 +1,11 @@
+package com.example.webservice_student_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceStudentAttendanceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceStudentAttendanceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/webservice_student_attendance/WebServiceStudentAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservice_student_attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceStudentAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
